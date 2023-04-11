b5fbbab4c09e53c88133016766c751b5a8eada35
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-11 19:59:11 -0300

    wip: adding textField component

---
+++ src/components/TextField/textField.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TextInputProps, TouchableOpacity, View } from 'react-native';
+import { theme } from '../../styles/styles';
+import { Feather } from '@expo/vector-icons';
+
+interface TextFieldProps extends TextInputProps{
+    label?:string,
+    isPassword?: boolean
+}
+
+
+const TextField = ({label, isPassword ,...props}: TextFieldProps): JSX.Element => {
+    const [isSecret, setIsSecret] = useState(isPassword)
+    return <View style={styles.container}>
+    <Text style={styles.label}>
+        Password
+    </Text>
+    <TextInput 
+        style={styles.containerTextInput}
+        {...props}
+        secureTextEntry={isSecret}
+    />
+    {isPassword && <>
+        <TouchableOpacity style={styles.icon} onPress={() => setIsSecret(!isSecret)}>
+            {isSecret ? <Feather name="eye" size={24} color={theme.colors.darkGrey}  />  : <Feather name="eye-off" size={24} color={theme.colors.darkGrey}  /> }
+        </TouchableOpacity>
+    </>}
+   
+    </View>;
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width:"100%",
+    },
+    containerTextInput: {
+        height: 48,
+        width:"100%",
+        borderRadius: 4,
+        backgroundColor: "#F4F4F4",
+        paddingHorizontal:10,
+        paddingVertical: 15
+    },
+    label: {
+        color: theme.colors.labelColor,
+        fontSize:11,
+        fontFamily: "500",
+        marginBottom:4,
+    },
+    icon: {
+        color: theme.colors.darkGrey,
+        position: "absolute",
+        right:10,
+        top:30
+    }
+})
+
+export default TextField;
