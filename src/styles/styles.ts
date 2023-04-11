eb6a5794de2e2d88b2171d4fca6556b30529dcf3
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-11 19:02:11 -0300

    wip: adding theme (colors)

---
+++ src/styles/styles.ts
@@ -0,0 +1,9 @@
+const theme = {
+    colors: {
+        colorPrimary: "#770FDF",
+        grey: "#F4F4F4",
+        darkGrey: "#A0A0A0",
+        green: "#0FDF8F",
+        orange: "#F0A719",
+    }
+}
