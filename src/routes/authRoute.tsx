dae7caa56dbcd7989420929960fa917e17dadf11
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-12 12:14:01 -0300

    wip: adding tab bottom navigation

---
+++ src/routes/authRoute.tsx
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import LoginPage from '../pages/loginPage';
+import RegisterPage from '../pages/registerPage';
+import MainRoute from './mainRoute';
+
+const Stack = createNativeStackNavigator();
+
+
+const AuthRoute = () => {
+  return  <Stack.Navigator>
+    <Stack.Screen name="Home" component={MainRoute} 
+        options={{
+            headerTitle:""
+        }}
+    />
+    <Stack.Screen name="Login"  component={LoginPage}   options={{
+      headerTitle:""
+    }} />
+    <Stack.Screen name="Register" component={RegisterPage} 
+      options={{
+        headerTitle:""
+      }}
+    />
+  </Stack.Navigator>;
+}
+
+export default AuthRoute;
---
+++ src/routes/mainRoute.tsx
@@ -0,0 +1,34 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+  
+  function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+  
+  const Tab = createBottomTabNavigator();
+
+const MainRoute = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+}
+
+export default MainRoute;
+
+
