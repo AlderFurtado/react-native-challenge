02301e5879b9f0df208fd9caeeb4b22fce1b0ea6
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-12 19:43:07 -0300

    wip: adding SelectSquare component

---
+++ src/components/SelectSquare/selectSquare.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import PlanText from '../PlanText/planText';
+import { theme } from '../../styles/styles';
+
+interface SelectSquareProps extends TouchableOpacityProps{
+    title:string,
+    isSelected: boolean
+}
+
+const SelectSquare = ({title,isSelected}:SelectSquareProps):JSX.Element => {
+  return <TouchableOpacity style={[styles.container,{backgroundColor: isSelected ? "#F7EFFF": theme.colors.while}]}>
+    <PlanText style={{color: isSelected ?  theme.colors.colorPrimary : theme.colors.darkGrey  }}>{title}</PlanText>
+  </TouchableOpacity>;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width:35,
+        height:35,
+        justifyContent:"center",
+        alignItems:"center",
+        padding:4
+    }
+})
+
+export default SelectSquare;
