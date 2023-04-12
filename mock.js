af3fbd18dc67ee18b71966ec15803267bcfcbed4
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-12 12:02:37 -0300

    wip: adding acount, funds mocks

--- mock.js
+++ mock.js
@@ -8,6 +8,32 @@ export const mock = {
         firstName: "Jonh",
         lastName: "Cena",
         isOver18: true,
+    },
+    account: {
+        portifolio: "1245.23",
+        bankAccount: "1457.23",
+        increaseByPorcent: "37"
+    },
+
+    funds: {
+        windFund: {
+            price: "10000",
+            increaseByPorcent: "3.4",
+            AUM: "4300000",
+            issueDate: "13/05/2023",
+            vintageRange: "2019-2022",
+            TER: "0.15",
+            priceAtClose: "17.48",
+            priceAtOpen: "17.44"
+        },
+        solarFund: {
+            price: "20000",
+            increaseByPorcent: "6.3"
+        },
+        naturalFund: {
+            price: "30000",
+            increaseByPorcent: "0.1"
+        },
     }
 
 }
