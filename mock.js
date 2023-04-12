e2d9353ae5de652878bb6fc73e40bf0cfb851aa3
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-11 23:31:52 -0300

    wip: adding mock userInfo

--- mock.js
+++ mock.js
@@ -1,4 +1,13 @@
 export const mock = {
-    loginEmail: "test@test.com",
-    loginPassword: "123456"
+    login: {
+        email: "test@test.com",
+        password: "123456"
+    },
+    userInfo: {
+        email: "test@test.com",
+        firstName: "Jonh",
+        lastName: "Cena",
+        isOver18: true,
+    }
+
 }
