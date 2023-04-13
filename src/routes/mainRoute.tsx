0f3df373bb4c8ce0ecfc53ac82e1b59668f308c7
alderfurtado26 <aldermarcel12@gmail.com>
2023-04-12 23:09:46 -0300

    fix: adding hearder trader

--- src/routes/mainRoute.tsx
+++ src/routes/mainRoute.tsx
@@ -7,11 +7,27 @@ import { Feather } from '@expo/vector-icons';
 import HomePage from '../pages/homePage';
 import FundDetailsPage from '../pages/fundDetailsPage';
 import PortfolioPage from '../pages/portifolioPage';
-
+import Title from '../components/Title/title';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import {  TouchableOpacity, View} from 'react-native';
+import PlanText from '../components/PlanText/planText';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 const Tab = createBottomTabNavigator();
 
+const HearderTrade = () => {
+    const fundInfo = useSelector((state: RootState) => state.fundInfo)
+    return <View style={{alignItems:"center"}}>
+     <Title>{fundInfo.name}</Title>
+     <PlanText>{fundInfo.acronym}</PlanText>
+    </View>
+}
+
 const MainRoute = () => {
+
+    const navigation = useNavigation()
     return (
         <Tab.Navigator
         screenOptions={({ route }) => ({
@@ -32,7 +48,14 @@ const MainRoute = () => {
                 headerShown: false
             }} />
              <Tab.Screen name="Trade" component={FundDetailsPage} options={{
-                headerShown: false
+                 headerTitle: (props) => <HearderTrade/>,
+                 headerLeft: () => (
+                    <TouchableOpacity onPress={() => {navigation.navigate("Main" as never)}}>
+                        <AntDesign name="arrowleft" size={24} color="black" style={{marginLeft:20}} />
+                    </TouchableOpacity>
+                    
+                  ),
+        
             }} />
             <Tab.Screen name="Portfolio" component={PortfolioPage} options={{
                 headerShown: false
